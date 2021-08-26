4051a54a3a4310b4804c590d264cb09c3a7d5fea
s0ooo0k <izz_m2@naver.com>
2021-08-26 15:44:42 +0900

    백 기본 세팅

--- EwhaFestival/settings.py
+++ EwhaFestival/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'festival',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
