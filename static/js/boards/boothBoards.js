7aa2a833d1bdddc04fb66968c1bb5a890a495cb9
park ye jin <dyddyd0514@naver.com>
2021-08-30 21:36:07 +0900

    [add] handle like button clicked function

--- static/js/boards/boothBoards.js
+++ static/js/boards/boothBoards.js
@@ -1,32 +1,52 @@
-const backButton=document.getElementById('back-button')
-function handleBackButtonClick(){
-    console.log("go-back clicked");
+const backButton = document.getElementById('back-button');
+function handleBackButtonClick() {
+  console.log('go-back clicked');
 }
-backButton.addEventListener("click", handleBackButtonClick);
+backButton.addEventListener('click', handleBackButtonClick);
 
-const searchButton=document.getElementById('search-button')
-function handleSearchButtonClick(){
-    console.log("search clicked");    
+const searchButton = document.getElementById('search-button');
+function handleSearchButtonClick() {
+  console.log('search clicked');
 }
-searchButton.addEventListener("click", handleSearchButtonClick);
+searchButton.addEventListener('click', handleSearchButtonClick);
 
-const themeHashtagButton=document.querySelectorAll("#theme-hashtag-button");
+const themeHashtagButton = document.querySelectorAll('#theme-hashtag-button');
 for (let i = 0; i < themeHashtagButton.length; i++) {
-    themeHashtagButton[i].addEventListener("click", function() {
-        console.log("hashtag button clicked");
-    });
+  themeHashtagButton[i].addEventListener('click', function () {
+    console.log('hashtag button clicked');
+  });
 }
 
-const likeBoothButton=document.querySelectorAll("#like-button");
+const likeBoothButton = document.querySelectorAll('#like-button');
 for (let i = 0; i < likeBoothButton.length; i++) {
-    likeBoothButton[i].addEventListener("click", function() {
-        console.log("like button clicked");
-    });
+  likeBoothButton[i].addEventListener('click', handleLikeButtonClicked);
 }
 
-const boothLabel=document.querySelectorAll("#booth-label");
+function handleLikeButtonClicked(event) {
+  if (event.target.src) {
+    if (event.target.src.indexOf('empty') !== -1) {
+      event.target.src = event.target.src.replace('empty', 'filled');
+    } else {
+      event.target.src = event.target.src.replace('filled', 'empty');
+    }
+  } else {
+    if (event.srcElement.innerHTML.indexOf('empty') !== -1) {
+      event.srcElement.innerHTML = event.srcElement.innerHTML.replace(
+        'empty',
+        'filled'
+      );
+    } else {
+      event.srcElement.innerHTML = event.srcElement.innerHTML.replace(
+        'filled',
+        'empty'
+      );
+    }
+  }
+}
+
+const boothLabel = document.querySelectorAll('#booth-label');
 for (let i = 0; i < boothLabel.length; i++) {
-    boothLabel[i].addEventListener("click", function() {
-        console.log("booth clicked");
-    });
+  boothLabel[i].addEventListener('click', function () {
+    console.log('booth clicked');
+  });
 }
