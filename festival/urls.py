69ef75efdf4109f4eb4e615e6d360d8d8e3a1acd
s0ooo0k <izz_m2@naver.com>
2021-09-01 13:16:16 +0900

    url static update

--- festival/urls.py
+++ festival/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings 
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('college/<str:college_id>/<int:pk_id>', views.detailcollegePost, name='detailcollegePost'),
     path('booth/', views.boardboothPost, name='boardboothPost'),
     path('booth/<int:booth_id>', views.detailboothPost, name='detailboothPost'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
