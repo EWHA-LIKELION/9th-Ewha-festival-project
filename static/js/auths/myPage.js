7aaa29a3fd1a829b2a3b7bc0012bd3b97fa9c527
park ye jin <dyddyd0514@naver.com>
2021-09-02 23:20:31 +0900

    [fix] background class name

--- static/js/auths/myPage.js
+++ static/js/auths/myPage.js
@@ -1,17 +1,25 @@
-const commentedPostListButton=document.getElementById('commented-post-list-button')
-function handleCommentedPostListButtonClick(){
-    console.log("commented-post-list clicked");
+const commentedPostListButton = document.getElementById('commented-post-list');
+function handleCommentedPostListButtonClick() {
+  console.log('commented-post-list clicked');
 }
-commentedPostListButton.addEventListener("click", handleCommentedPostListButtonClick);
+commentedPostListButton.addEventListener(
+  'click',
+  handleCommentedPostListButtonClick
+);
 
-const commentedBoothListButton=document.getElementById('commented-booth-list-button')
-function handleCommentedBoothListButtonClick(){
-    console.log("commented-booth-list clicked");
+const commentedBoothListButton = document.getElementById(
+  'commented-booth-list'
+);
+function handleCommentedBoothListButtonClick() {
+  console.log('commented-booth-list clicked');
 }
-commentedBoothListButton.addEventListener("click", handleCommentedBoothListButtonClick);
+commentedBoothListButton.addEventListener(
+  'click',
+  handleCommentedBoothListButtonClick
+);
 
-const likedBoothListButton=document.getElementById('liked-booth-list-button')
-function handleLikedBoothListButtonClick(){
-    console.log("liked-booth-list clicked");
+const likedBoothListButton = document.getElementById('liked-booth-list');
+function handleLikedBoothListButtonClick() {
+  console.log('liked-booth-list clicked');
 }
-likedBoothListButton.addEventListener("click", handleLikedBoothListButtonClick);
+likedBoothListButton.addEventListener('click', handleLikedBoothListButtonClick);
