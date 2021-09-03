6910dc8b0934e29d3a55a5114eca4ab535981e81
park ye jin <dyddyd0514@naver.com>
2021-09-04 00:18:00 +0900

    [add] college to common.js

--- static/js/boards/collegeBoards.js
+++ static/js/boards/collegeBoards.js
@@ -1,18 +1,3 @@
-const college = {
-  humanities: '인문과학대학',
-  social: '사회과학대학',
-  natural: '자연과학대학',
-  engineering: '엘텍공과대학',
-  music: '음악대학',
-  art: '조형예술대학',
-  edu: '사범대학',
-  business: '경영대학',
-  convergence: '신산업융합대학',
-  nursing: '간호대학',
-  scraton: '스크랜튼대학',
-  hokma: '호크마교양대학',
-  pharmacy: '약학대학',
-};
 const majorHashTag = document.querySelectorAll('.major-hashtag-white');
 function handleHashtagClicked(event) {
   console.log(event.target.id);
