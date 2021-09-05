51db9c99f138b4af29379794581742181694528e
TaeeunKim <xodms0309@naver.com>
2021-09-05 23:15:00 +0900

    style: 회원가입 css 변경

--- EwhaFestival/settings.py
+++ EwhaFestival/settings.py
@@ -53,12 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-]
 
 ROOT_URLCONF = 'EwhaFestival.urls'
 
