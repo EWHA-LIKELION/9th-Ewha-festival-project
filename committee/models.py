fc7a57ce0fafdef9daa5fd6a43e92dc0aeb6e975
s0ooo0k <izz_m2@naver.com>
2021-09-07 23:20:14 +0900

    edit committee

--- committee/models.py
+++ committee/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.constraints import UniqueConstraint
 from django.db.models.fields import BLANK_CHOICE_DASH, DateField, TextField
 from django.db.models.fields.files import ImageField
-from django.contrib.auth.models import User
+from account.models import Profile
 from django.utils import timezone
 
 # Create your models here.
@@ -10,9 +10,6 @@ class committeePost(models.Model):
     title = models.CharField(max_length=50)
     pub_time = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
-
-    # 저장하기
-    committee_like = models.ManyToManyField(User, related_name='committee_like', blank=True)
 
     def __str__(self): 
         return self.title
@@ -27,7 +24,7 @@ class committeeImage(models.Model):
 class committeeComment(models.Model):
     post = models.ForeignKey(committeePost, on_delete=models.CASCADE, null=True, related_name='comments')
     comment_contents = models.TextField()
-    comment_writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    comment_writer = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     comment_date = models.DateTimeField(default=timezone.now)
 
     def approve(self):
